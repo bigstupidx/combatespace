ce71a79e1763173bb1465a98cb25957027f8e45e
pontura <pontura@gmail.com>
2016-07-14 16:56:15 -0300

    UI

---
+++ Assets/DebugClass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugClass : MonoBehaviour {
+
+	public Animation anim;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            Idle();
+        else  if (Input.GetKeyDown(KeyCode.W))
+            Defense();
+    }
+    void Idle()
+    {
+        print("Idle");
+        anim.CrossFade("defense_r", 0.15f);
+    }
+    void Defense()
+    {
+        print("Defense");
+        anim.CrossFade("defense_l", 0.15f);
+    }
+}
